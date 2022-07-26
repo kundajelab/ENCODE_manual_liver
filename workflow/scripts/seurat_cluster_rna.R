04669b0b8552ed934a83ac00c3f22b0d895445f3
Austin <austin.wang1357@gmail.com>
2022-07-25 20:20:07 -0700

    debug

--- workflow/scripts/seurat_cluster_rna.R
+++ workflow/scripts/seurat_cluster_rna.R
@@ -18,6 +18,7 @@ set.seed(params[["seed"]])
 
 proj <- readRDS(file = input_paths[["project_rna"]])
 ref <- readRDS(file = input_paths[["project_ref"]])
+print(head(ref@meta.data)) ####
 ref <- subset(x = ref, subset = `Factor.Value.inferred.cell.type...authors.labels.` != NA)
 
 anchors <- FindTransferAnchors(
