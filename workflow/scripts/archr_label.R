226c81130e6dbb0725eeebf34a20383f75273746
Austin <austin.wang1357@gmail.com>
2022-08-03 15:24:56 -0700

    debug

--- workflow/scripts/archr_label.R
+++ workflow/scripts/archr_label.R
@@ -46,6 +46,7 @@ proj <- saveArchRProject(
 ##########
 # head(read.table(file = input_paths[["label_data"]], sep = '\t', header = FALSE)) ####
 clustdata <- read.table(file = input_paths[["label_data"]], sep = '\t', header = FALSE, row.names = 1, comment.char = "")
+head(clustdata) ####
 
 cellnames <- row.names(clustdata)
 proj_index <- match(getCellNames(ArchRProj = proj), cellnames)
