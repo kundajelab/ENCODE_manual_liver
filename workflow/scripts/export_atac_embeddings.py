f7a6e37b7a33f9024ffff73a1ce04115b9220cf8
Austin <austin.wang1357@gmail.com>
2022-08-13 16:04:45 -0700

    debug

---
+++ workflow/scripts/export_atac_embeddings.py
@@ -0,0 +1,38 @@
+HEADER_EMB = """
+# Unified cell embeddings integrated using Harmony
+# cell_id: Cell ID used in integrated analysis
+# LSI1, LSI2, … LSI30: Columns of a 30-dimensional Harmony embedding vector
+"""
+
+HEADER_UMAP = """
+# UMAP coordinates of each cell
+# cell_id: Cell ID used in integrated analysis
+# UMAP_1, UMAP_2: UMAP x and y coordinates, respectively
+"""
+
+def main(emb_in_path, umap_in_path, emb_out_path, umap_out_path):
+    with open(emb_in_path) as f, open(emb_out_path, "w") as fo:
+        fo.write(HEADER_EMB)
+
+        h = f.readline()
+        fo.write("cell_id" + h)
+
+        for line in f:
+            fo.write(line)
+
+    with open(umap_in_path) as f, open(umap_out_path, "w") as fo:
+        fo.write(HEADER_UMAP)
+
+        h = f.readline()
+        fo.write("cell_id" + h)
+
+        for line in f:
+            fo.write(line)
+
+emb_in_path = snakemake.input["emb"]
+umap_in_path = snakemake.input["umap"]
+
+emb_out_path = snakemake.output["emb"]
+umap_out_path = snakemake.output["umap"]
+
+main(emb_in_path, umap_in_path, emb_out_path, umap_out_path)
